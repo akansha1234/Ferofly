153ae6baf3399edb796f2e12c8da75ed9aaa882d
akansha1234 <akanshagoel5997@gmail.com>
2020-12-24 17:18:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,222 @@
+function Header() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "main" }, /*#__PURE__*/
+    React.createElement("nav", { className: "navbar navbar-inverse navbar-fixed-top" }, /*#__PURE__*/
+    React.createElement("div", { className: "container-fluid" }, /*#__PURE__*/
+    React.createElement("div", { className: "d-flex nav" }, /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "HOME"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "ABOUT"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "ROOMS"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "SERVICES"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "GALLERY"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "SEE & DO"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "CONTACT"), /*#__PURE__*/
+    React.createElement("button", { className: "btn book" }, "Book A Room"))))));
+
+
+
+
+
+}
+
+function Heading() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "heading" }, /*#__PURE__*/
+    React.createElement("h1", null, /*#__PURE__*/React.createElement("i", null, "Ferofly")), /*#__PURE__*/
+    React.createElement("h3", null, /*#__PURE__*/React.createElement("i", null, "-Magnify your journey-"))));
+
+
+}
+function Form() {
+  return /*#__PURE__*/(
+    React.createElement("form", { className: "form" }, /*#__PURE__*/
+    React.createElement("div", { className: "form-row no-gutters" }, /*#__PURE__*/
+    React.createElement("div", { className: "form-group col-sm-3 col-lg-3 padding-0" }, /*#__PURE__*/
+    React.createElement("p", null, "Check In"), /*#__PURE__*/
+    React.createElement("input", { type: "date" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "form-group col-sm-3  col-lg-3 padding-0" }, /*#__PURE__*/
+    React.createElement("p", null, "Check Out"), /*#__PURE__*/
+    React.createElement("input", { type: "date" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "form-group col-sm-2 padding-0" }, /*#__PURE__*/
+    React.createElement("p", null, "Adults"), /*#__PURE__*/
+    React.createElement("input", { type: "number", min: "1", max: "100", value: "1" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "form-group col-sm-2 padding-0" }, /*#__PURE__*/
+    React.createElement("p", null, "Kids"), /*#__PURE__*/
+    React.createElement("input", { type: "number", min: "1", max: "100", value: "0" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-2 search padding-0" }, /*#__PURE__*/
+    React.createElement("button", { className: "btn btn-lg search" }, "Search")))));
+
+
+
+
+}
+function About() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "about" }, /*#__PURE__*/
+    React.createElement("h5", null, "ABOUT"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me. It\u2019s easy. Just click ", /*#__PURE__*/React.createElement("br", null), "\u201CEdit Text\u201D or double click me to add your own content and make changes to the ", /*#__PURE__*/React.createElement("br", null), "font. Feel free to drag and drop me anywhere you like on your page. I\u2019m a great", /*#__PURE__*/React.createElement("br", null), " place for you to tell a story and let your users know a little more about you."))));
+
+
+}
+function Rooms() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "rooms" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://images.unsplash.com/photo-1560067174-e553b3647603?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80", className: "image1" }), /*#__PURE__*/
+    React.createElement("div", { className: "room" }, /*#__PURE__*/
+    React.createElement("h5", null, "OUR ROOMS"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me. I\u2019m a great place for you to tell a story and let your users know a little more about you.")), /*#__PURE__*/
+
+
+    React.createElement("button", { className: "btn btn-rooms" }, "Book A Room"))));
+
+
+
+}
+function Services() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "services" }, /*#__PURE__*/
+    React.createElement("h5", null, "OUR SERVICES"), /*#__PURE__*/
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("i", { class: "fas fa-coffee" }), /*#__PURE__*/
+    React.createElement("h6", null, "Breakfast"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("i", { className: "fas fa-tree" }), /*#__PURE__*/
+    React.createElement("h6", null, "Garden"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("i", { className: "fas fa-water" }), /*#__PURE__*/
+    React.createElement("h6", null, "Waterfall"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me.")))), /*#__PURE__*/
+
+
+
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("i", { class: "fa fa-wifi" }), /*#__PURE__*/
+    React.createElement("h6", null, "Free Wifi"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("i", { class: "fas fa-tshirt" }), /*#__PURE__*/
+    React.createElement("h6", null, "Daily Housekeeping"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("i", { class: "fas fa-concierge-bell" }), /*#__PURE__*/
+    React.createElement("h6", null, "In-Room Dining Service"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))))));
+
+
+
+
+}
+function Gallery() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "gallery" }, /*#__PURE__*/
+    React.createElement("h5", null, "GALLERY"), /*#__PURE__*/
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://images.unsplash.com/photo-1560920452-dcdcbbb4e117?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80", className: "image2" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://images.unsplash.com/photo-1573836469204-9386d81a67a9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80", className: "image2" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTN8fGhvdGVsc3xlbnwwfDF8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", className: "image2" })))));
+
+
+
+
+}
+function Things() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "things" }, /*#__PURE__*/
+    React.createElement("h5", null, "THINGS TO DO "), /*#__PURE__*/
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("h6", null, "Wineries Tour"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("h6", null, "Cultural Sites"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("h6", null, "Market Tour "), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me.")))), /*#__PURE__*/
+
+
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("h6", null, "Leisure Activities"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("h6", null, "Birds Safari"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-4" }, /*#__PURE__*/
+    React.createElement("h6", null, "Horse Riding "), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "I'm a paragraph. Click here to add your own text and edit me."))))));
+
+
+
+
+}
+function Review() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "review" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://images.unsplash.com/photo-1557808051-a7642867211c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Z2FyZGVuJTIwd2l0aCUyMGZsb3dlcnN8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", className: "image1" }), /*#__PURE__*/
+    React.createElement("div", { className: "room" }, /*#__PURE__*/
+    React.createElement("h5", null, "GUEST REVIEW"), /*#__PURE__*/
+    React.createElement("p", { className: "aboutPara" }, /*#__PURE__*/React.createElement("i", null, "\u201CI'm a testimonial. Click to edit me and add text that says something nice about you and your services.\u201D")))));
+
+
+
+
+
+}
+function App() {
+  return /*#__PURE__*/(
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement(Header, null), /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col-sm-1" }, /*#__PURE__*/
+    React.createElement("div", { className: "left" }, /*#__PURE__*/
+    React.createElement("i", { class: "far fa-comment-dots" }))), /*#__PURE__*/
+
+
+    React.createElement("div", { className: "col-sm-10" }, /*#__PURE__*/
+    React.createElement(Heading, null), /*#__PURE__*/
+    React.createElement("img", { src: "https://images.unsplash.com/photo-1559252669-dab0e95b90f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80", className: "image1" }), /*#__PURE__*/
+    React.createElement(Form, null), /*#__PURE__*/
+    React.createElement(About, null), /*#__PURE__*/
+    React.createElement(Rooms, null), /*#__PURE__*/
+    React.createElement(Services, null), /*#__PURE__*/
+    React.createElement(Gallery, null), /*#__PURE__*/
+    React.createElement(Things, null), /*#__PURE__*/
+    React.createElement(Review, null)), /*#__PURE__*/
+
+    React.createElement("div", { className: "col-sm-1" }, /*#__PURE__*/
+    React.createElement("div", { className: "right" }, /*#__PURE__*/
+    React.createElement("i", { class: "fab fa-tripadvisor" }), /*#__PURE__*/
+    React.createElement("i", { class: "fab fa-facebook-f" }), /*#__PURE__*/
+    React.createElement("i", { class: "fab fa-instagram" })))))));
+
+
+
+
+
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
